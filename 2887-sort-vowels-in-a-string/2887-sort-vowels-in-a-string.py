e538437b138a71df247460b56cde1e679f88cecc
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-11-13 09:22:02 +0100

    Time: 238 ms (22.73%), Space: 28.7 MB (6.34%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
@@ -8,15 +8,13 @@ class Solution:
         for i in range(len(s)):
             if s[i] in vowels:
                 idx_to_change.add(i)
-                letters_to_change.append(s[i])
+                heapq.heappush(letters_to_change, s[i])
             t[i] = s[i]
-
-        letters_to_change.sort()    
 
         letter_count = 0
         for i in range(len(s)):
             if i in idx_to_change:
-                t[i] = letters_to_change[letter_count]
+                t[i] = heapq.heappop(letters_to_change)
                 letter_count += 1
         
         return ''.join(t)
