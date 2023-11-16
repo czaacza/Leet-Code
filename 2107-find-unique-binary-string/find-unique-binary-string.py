256b32e4e8b8c5f5501a618f59a322b3c2afdac5
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-11-16 10:43:13 +0100

    Time: 826 ms (9.69%) | Memory: 16.5 MB (14.04%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,22 +1,21 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         nums_set = set(nums)
-        res = set()
+        res = None
         def backtrack(curr_str, curr_len):
             nonlocal res
 
             if curr_len == len(nums[0]):
-                res.add(curr_str)
+                if curr_str not in nums_set:
+                    res = curr_str
                 return
             
             backtrack(curr_str + "0", curr_len+1)
             backtrack(curr_str + "1", curr_len+1)
         
         backtrack('', 0)
-
-        for el in res:
-            if el not in nums_set:
-                return el
+        
+        return res
            
 
 
