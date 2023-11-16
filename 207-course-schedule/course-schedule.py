8270225ff9ac526e6ffb7016b552e01e807e0158
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-11-16 20:27:53 +0100

    Time: 98 ms (61.86%) | Memory: 19.2 MB (23.26%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        adj = {}
+        for el in prerequisites:
+            adj[el[0]] = adj.get(el[0], []) + [el[1]]
+        
+        print('adj', adj)
+
+        canAllBeDone = True
+        def dfs(curr, visited): 
+            nonlocal canAllBeDone
+            if curr not in adj or adj[curr] == []:
+                return
+
+            if curr in visited:
+                canAllBeDone = False
+                return
+            visited.add(curr)
+
+            for n in adj[curr]:
+                dfs(n, visited)
+            
+            visited.remove(curr)
+            adj[curr] = []
+
+
+        for n in range(numCourses):
+            dfs(n, set())
+            if canAllBeDone == False:
+                return False
+
+        return True
+
+
+                        
+        
