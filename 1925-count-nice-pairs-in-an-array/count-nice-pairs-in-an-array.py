9c78539caa2217572dd9b8ffcc2907cd6b27c49c
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-11-21 13:30:28 +0100

    Time: 661 ms (22.66%) | Memory: 26.8 MB (61.19%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
@@ -1,14 +1,16 @@
 class Solution:
     def countNicePairs(self, nums: List[int]) -> int:
-        def rev(s):
-            reversed_str = ''
-            for i in range(len(s)-1, -1, -1):
-                reversed_str += s[i]
-            return int(reversed_str)
+        def rev(num):
+            result = 0
+            while num:
+                result = result * 10 + num % 10
+                num //= 10
+            
+            return result
 
         rev_values = []
         for i in range(len(nums)):
-            rev_values.append(nums[i] - rev(str(nums[i])))
+            rev_values.append(nums[i] - rev(nums[i]))
 
         seen = {}
         res = 0
@@ -20,17 +22,4 @@ class Solution:
             res += seen[val]
             seen[val] += 1
 
-        return res % (10**9 + 7)
-
-            
-
-
-
-
-                
-
-
-
-
-
-        
+        return res % (10**9 + 7)       
