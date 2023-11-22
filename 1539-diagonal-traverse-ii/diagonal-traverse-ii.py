2a214a2c7fe460f623c5a6751e22f4b77e615bc5
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-11-22 17:49:20 +0100

    Time: 4470 ms (5.02%) | Memory: 503 MB (5.02%) - LeetSync

---
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        ROWS = len(nums)
+        COLS = len(nums[0])
+        max_sum = (ROWS + COLS) * 1000
+        
+        sums = [-1] * max_sum
+
+        for r in range(ROWS-1, -1, -1):
+            for c in range(len(nums[r])-1, -1, -1):
+                suma = r + c
+                if sums[suma] == -1:
+                    sums[suma] = []
+                sums[suma].append(nums[r][c])
+
+        res = []
+        for arr in sums:
+            if arr == -1:
+                continue
+            for el in arr:
+                res.append(el)
+
+        return res
+
+
+        
