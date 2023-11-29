638af810792f931f91da3ae5f8d7e31f742870db
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-11-29 23:40:37 +0100

    Time: 13 ms (14.17%) | Memory: 40.8 MB (5.35%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,47 @@
+class Solution {
+    
+    public int rec(int n, int[] nums){
+        if (n == 1){
+            return 1;
+        }
+        int i = 1;
+        int total = 0;
+        
+        while (i <= n){
+            int left_len = i == 1 ? 1 : i-1;
+            int right_len = i == n ? 1 : n - i;
+            
+            int left_val = -1;
+            int right_val = -1;
+            
+            if (nums[left_len] != -1){
+                left_val = nums[left_len];
+            } else{
+                left_val = rec(left_len, nums);
+            }
+            
+            if (nums[right_len] != -1){
+                right_val = nums[right_len];
+            } else{
+                right_val = rec(right_len, nums);
+            }
+            
+            total += (left_val * right_val);
+            i++;
+        }
+        
+        nums[n] = total;
+        System.out.println("\nnums");
+        for (i = 0; i < n; i ++){
+            System.out.print(nums[i] + " ");
+        }
+        return total;
+    }
+    
+    public int numTrees(int n) {
+        int[] nums = new int[n + 1];
+        Arrays.fill(nums, -1);
+        
+        return rec(n, nums); 
+    }
+}
