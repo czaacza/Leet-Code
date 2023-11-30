7011fb9c54eecb2092978a777b4e34d9d747b514
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-11-30 19:22:51 +0100

    Time: 91 ms (5.12%) | Memory: 17.3 MB (35.44%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ''
+        for l in s.lower():
+            print('l', ord(l))
+            if ord(l) >= 97 and ord(l) <= 122 or ord(l) >= 48 and ord(l) <= 57:
+                res += l
+        print('res', res)
+        for i in range(len(res)):
+            if res[i] != res[-1-i]:
+                return False
+        return True
+            
+            
+            
+            
+        
