0a7244325f026aad00f2e94b18663d39d720a561
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-12-01 14:39:20 +0100

    Time: 44 ms (27.14%) | Memory: 16.4 MB (28.02%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
@@ -3,11 +3,9 @@ class Solution:
         
         str1, str2 = "", ""
         for word in word1:
-            for l in word:
-                str1 += l
+                str1 += word
         for word in word2:
-            for l in word:
-                str2 += l
+                str2 += word
         if str1 == str2:
             return True
         return False
