559432295e698509d3fe0dafacaa074d73e5bece
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-12-07 17:17:31 +0100

    Time: 46 ms (8.92%) | Memory: 16.4 MB (34.55%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,45 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        
+        mapping = [
+            [],
+            [], 
+            ['a', 'b', 'c'],
+            ['d', 'e', 'f'],
+            ['g', 'h', 'i'],
+            ['j', 'k', 'l'],
+            ['m', 'n', 'o'],
+            ['p', 'q', 'r', 's'],
+            ['t', 'u', 'v'],
+            ['w', 'x', 'y', 'z']
+            ]
+        
+        letters = []
+        for d in digits:
+            letters.append(mapping[int(d)])
+        
+        # print('letters', letters)
+        res = []
+        
+        def comb(letters, res, lvl, curr_str):
+            # print('comb, curr_str', curr_str)
+            # print('lvl', lvl)
+            if lvl == len(letters):
+                res.append(curr_str)
+                return
+            
+            for letter in letters[lvl]:
+                comb(letters, res, lvl + 1, curr_str + letter)        
+        
+        for letter in letters[0]:
+            comb(letters, res, 1, letter)
+            
+        
+        return res
+            
+
+        
+                
+
