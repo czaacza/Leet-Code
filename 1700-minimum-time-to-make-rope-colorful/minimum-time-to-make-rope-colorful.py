fbd532296849414113a638164b605b59f1e67468
Mateusz Czarnecki <93872997+czaacza@users.noreply.github.com>
2023-12-28 14:18:16 +0100

    Time: 871 ms (77.84%) | Memory: 28.1 MB (10.18%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,19 +1,20 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        l, r = 0, 0
+        l, r = 0,0 
         res = 0
+
         while r < len(colors):
-            curr_heap = []
-            while r < len(colors) and colors[r] == colors[l]:
-                heapq.heappush(curr_heap, neededTime[r])
+            curr_sum = 0
+            max_time = 0
+            while r < len(colors) and colors[l] == colors[r]:
+                curr_sum += neededTime[r]
+                max_time = max(max_time, neededTime[r])
                 r += 1
-            found = heapq.nsmallest(r - l - 1, curr_heap)
-            for f in found:
-                res += f  
 
-            l = r        
+            curr_sum -= max_time
+            res += curr_sum
 
+            l = r
+        
         return res
-
-
         
